2ba8f4d5cb7086d5ea63325bb2e2608f72a57fed
Praveenjayathissa <praveenajayathis@gmail.com>
2026-07-24 16:22:08 +0530

    Add database connection utility

---
+++ src/main/java/com/airline/database/TestConnection.java
@@ -0,0 +1,23 @@
+package com.airline.database;
+
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection connection = DBConnection.getConnection();
+
+        if (connection != null) {
+
+            System.out.println("Database Connected Successfully!");
+
+        } else {
+
+            System.out.println("Database Connection Failed!");
+
+        }
+
+    }
+
+}
