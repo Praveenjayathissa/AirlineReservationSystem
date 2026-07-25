8157335f7956af277bf485bb074c79b2453656d5
achiradilshan <dilshanachira4@gmail.com>
2026-07-25 14:24:20 +0530

    add user model

---
+++ src/main/java/com/airline/model/User.java
@@ -0,0 +1,72 @@
+package com.airline.model;
+
+public class User {
+
+    private int userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String passportNo;
+
+    public User() {
+    }
+
+    public User(int userId, String fullName, String email, String password,
+                String phone, String passportNo) {
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.passportNo = passportNo;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassportNo() {
+        return passportNo;
+    }
+
+    public void setPassportNo(String passportNo) {
+        this.passportNo = passportNo;
+    }
+}
